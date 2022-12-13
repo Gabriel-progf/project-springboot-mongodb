8213d8e73f73a90dcfb50c3e86f05cd7d4a17778
Gabriel <gabriel.ol.macedo@hotmail.com>
2022-12-12 21:47:40 -0300

    Added UserDto

---
+++ src/main/java/com/Projectgb/projectmongo/dto/UserDto.java
@@ -0,0 +1,27 @@
+package com.Projectgb.projectmongo.dto;
+
+import java.io.Serializable;
+
+import com.Projectgb.projectmongo.domain.User;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    private String id;
+    private String name;
+    private String email;
+
+    public UserDto(User user){
+        id = user.getId();
+        name = user.getName();
+        email = user.getEmail();
+    }
+}
--- src/main/java/com/Projectgb/projectmongo/resources/UserResource.java
+++ src/main/java/com/Projectgb/projectmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.Projectgb.projectmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Projectgb.projectmongo.domain.User;
+import com.Projectgb.projectmongo.dto.UserDto;
 import com.Projectgb.projectmongo.services.UserService;
 
 @RestController
@@ -19,8 +21,9 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDto>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
