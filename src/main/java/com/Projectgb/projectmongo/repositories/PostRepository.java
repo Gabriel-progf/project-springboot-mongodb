affc1021fc5667ede33411691168a8e9fa8c28d0
Gabriel <gabriel.ol.macedo@hotmail.com>
2022-12-14 20:47:07 -0300

    Added query method

--- src/main/java/com/Projectgb/projectmongo/repositories/PostRepository.java
+++ src/main/java/com/Projectgb/projectmongo/repositories/PostRepository.java
@@ -1,9 +1,12 @@
 package com.Projectgb.projectmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.Projectgb.projectmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>  {
-    
+
+    List<Post> findByTitleContainingIgnoreCase(String title);
 }
