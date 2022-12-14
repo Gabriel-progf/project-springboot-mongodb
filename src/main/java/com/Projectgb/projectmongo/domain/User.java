6ca146b1065d24e3048a12726278d6775efa2092
Gabriel <gabriel.ol.macedo@hotmail.com>
2022-12-14 20:05:28 -0300

    Reference posts from user

--- src/main/java/com/Projectgb/projectmongo/domain/User.java
+++ src/main/java/com/Projectgb/projectmongo/domain/User.java
@@ -1,17 +1,19 @@
 package com.Projectgb.projectmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.AllArgsConstructor;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
@@ -23,7 +25,19 @@ public class User implements Serializable {
 
     @Id
     private String id;
-    private String name;   
+    private String name;
     @EqualsAndHashCode.Include
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    
+
 }
